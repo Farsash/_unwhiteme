4c407c252896141aef345bfc8ea01cdef4d40e28
farsash2007 <farsash2007@gmail.com>
2018-12-26 14:58:34 +0300

    file saver

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
 var express = require('express');
 var app = express();
 var sqlite3 = require('sqlite3').verbose();
+var bodyParser = require('body-parser');
+const multer = require('multer');
+const path = require('path');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + '.jpg')
+    }
+  })
+  
+var upload = multer({ storage: storage });
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set('view engine', 'ejs');
+app.use('/assets', express.static('public'));
+
+app.get('/', function (req, res) {
+    res.render('index', { name:req.params.id });
+});
+
+app.post('/upload', upload.single('avatar'), function (req, res, next) {
+    console.log('res');
+});
+
+
+
+
+
+app.get('/test/:id', function (req, res) {
+    res.render('index', { name:req.params.id });
+});
+
 
 function ReadDB(){
     
@@ -15,9 +49,12 @@ function ReadDB(){
     db.close();
 }
 
-app.get('/n/:id', function (req, res) {
-    res.render('index', { name:req.params.id });
-  });
+
+
+
+
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
